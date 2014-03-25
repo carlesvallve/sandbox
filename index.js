f11b25bd1e9148b1a6b1cef9f2f6ba8382260e39
Carles Vallve <snaptothegrid@gmail.com>
2014-03-26 03:59:25 +0900

    Created project basic structure + jshint

---
+++ index.js
@@ -0,0 +1,43 @@
+var HelloButton = require('helloButton');
+var aButton = new HelloButton();
+aButton.appendTo(document.body);
+
+function welcome() {
+    console.log('Hello Component!');
+}
+
+welcome();
+
+/*
+- create a script loader
+- create an asset loader
+- create a basic navTree + view
+- integrate wuic, or even better, create our own canvas render system
+- create our own tweener, or use public components tween + ease
+ */
+
+/* PUBLIC COMPONENTS */
+// component / inherit
+// component-uglifyjs
+// component-less
+
+
+/* PUBLIC COMPONENTS */
+
+// events
+// eventemitter
+// Recorder
+// math-random
+// SoundManager2
+// math-...
+// list-...
+// noise-...
+
+// csvparser
+// mobileVersionDetection
+
+// gettextlight.js
+
+/* PUBLIC NODE.JS COMPONENTS */
+// Builder
+// node-tomes
